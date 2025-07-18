afe0423d6b0abd69b17206c882ec4e7c8d9bee01
pmpsites <markpartnerpmp@gmail.com>
2025-07-18 16:03:17 +0100

    add .min to styles.min.css  CMS.registerPreviewStyle

--- static/admin/cms.js
+++ static/admin/cms.js
@@ -262,4 +262,4 @@ const InsightPreview = createClass({
 });
 
 CMS.registerPreviewTemplate("insights", InsightPreview);
-CMS.registerPreviewStyle('https://place-marketing.com/css/styles.css'); 
+CMS.registerPreviewStyle('https://place-marketing.com/css/styles.min.css'); 
